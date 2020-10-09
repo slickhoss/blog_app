53cc49b9544565c2545925537231c2fdc6efc7a3
Andre Hoong <hoongandre@gmail.com>
2020-10-09 15:09:51 -0700

    Implementing test for no articles present

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Listing Articles' do
         @article2 = Article.create(title: 'article2', body:'Lorem ipsum dolor sit amet')
     end
 
-    scenario 'User lists all articles' do
+    scenario 'App lists all articles' do
         visit '/'
         expect(page).to have_content('This is the articles page')
         expect(page).to have_content(@article1.title)
@@ -16,7 +16,23 @@ RSpec.feature 'Listing Articles' do
         expect(page).to have_content(@article2.body)
         expect(page).to have_link(@article1.title)
         expect(page).to have_link(@article2.title)
-   
+    end
+
+
+    scenario 'App lists no articles' do
+        Article.delete_all
+        visit '/'
+        expect(page).to have_content('This is the articles page')
+        expect(page).not_to have_content(@article1.title)
+        expect(page).not_to have_content(@article1.body)
+        expect(page).not_to have_content(@article2.title)
+        expect(page).not_to have_content(@article2.body)
+        expect(page).not_to have_link(@article1.title)
+        expect(page).not_to have_link(@article2.title)
+
+        within("h1#no-articles") do
+            expect(page).to have_content('No Articles Created')
+        end
     end
 
 
