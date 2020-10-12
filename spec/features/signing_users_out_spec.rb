d0efea6bc181462f8db1c1ac8868ecf5e2386275
Andre Hoong <hoongandre@gmail.com>
2020-10-12 13:29:56 -0700

    Complete signout test

--- spec/features/signing_users_out_spec.rb
+++ spec/features/signing_users_out_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.feauture 'Signing out logged in users' do
+RSpec.feature 'Signing out logged in users' do
     before do
         @john = User.create(email: 'demo@example.com', password: 'password')
         visit '/'
-        click_link 'Sign in'
+        click_link 'Sign In'
         fill_in 'Email', with: @john.email
         fill_in 'Password', with: @john.password
         click_button 'Log in'
@@ -12,8 +12,8 @@ RSpec.feauture 'Signing out logged in users' do
 
     scenario 'Sign out' do
         visit '/'
-        click_link 'Sign ot'
+        click_link 'Sign Out'
         expect(page).to have_content('Signed out successfully.')
-        expect(page).not_to have_content('Sign out')
+        expect(page).not_to have_content('Sign Out')
     end
 end
