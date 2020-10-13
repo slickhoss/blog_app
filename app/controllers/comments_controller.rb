c86a6ea0da4e97039d0a0e23c8196d098259b685
Andre Hoong <hoongandre@gmail.com>
2020-10-13 00:03:57 -0700

    Real time comments implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,10 @@ class CommentsController < ApplicationController
             @comment = @article.comments.build(comment_params) 
             @comment.user = current_user
             if @comment.save
+                ActionCable.server.broadcast "comments",
+                render(partial: 'comments/comment', object: @comment ) 
                 flash[:notice] = "Comment has been created"
-                redirect_to article_path(@article)
+                #redirect_to article_path(@article)
             else
                 flash.now[:alert] = "Comment has not been created"
                 redirect_to new_user_session_path
